3d2178d3b34807e6eb917e0078e00db068e60184
AlexWambui <alexwambuik@gmail.com>
2026-07-08 14:27:09 +0300

    added the navbar.

--- resources/js/layouts/GuestLayout.vue
+++ resources/js/layouts/GuestLayout.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-
+import Navbar from '@/components/custom/Navbar/Index.vue';
 </script>
 
 <template>
+    <Navbar />
+    
     <main class="GuestLayout">
         <slot />
     </main>
