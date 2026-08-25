de1fba7916bae18399e1c90bdeef33387ac178ae
AlexWambui <alexwambuik@gmail.com>
2026-08-25 16:19:12 +0300

    added the user module.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,11 @@
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
 
+use Modules\User\Providers\UserServiceProvider;
+
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
+
+    UserServiceProvider::class,
 ];
